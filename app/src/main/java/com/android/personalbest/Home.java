73a9c3b9033b0f3d2572ee3e227d4db4ea63a1ef
Lola Liu <lola@Lolas-MBP.attlocal.net>
2019-02-05 20:13:36 -0800

    Home and Tracking Page UI Design

--- app/src/main/java/com/android/personalbest/Home.java
+++ app/src/main/java/com/android/personalbest/Home.java
@@ -1,10 +1,13 @@
 package com.android.personalbest;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class HomeBtmNavActivity extends AppCompatActivity {
@@ -39,6 +42,21 @@ public class HomeBtmNavActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        Button startbtn = (Button) findViewById(R.id.start);
+        startbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchActivity();
+            }
+        });
     }
+
+    private void launchActivity() {
+        Intent intent = new Intent(this, trackerActivity.class);
+        startActivity(intent);
+    }
+
+
 
 }
