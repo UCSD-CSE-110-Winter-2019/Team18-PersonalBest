37e3bcaa26cbb2535509a0bfbba7586fb5afda6e
Christopher Guan <cwguan@ucsd.edu>
2019-03-08 19:42:01 -0800

    Updated LoginUI/GetToKnowYouUI to use firestore

--- app/src/main/java/com/android/personalbest/firestore/IFirestore.java
+++ app/src/main/java/com/android/personalbest/firestore/IFirestore.java
@@ -4,7 +4,10 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.android.personalbest.MainActivity;
+import com.android.personalbest.UIdisplay.GetToKnowYouUI;
 import com.android.personalbest.UIdisplay.HomeUI;
+import com.android.personalbest.UIdisplay.LoginUI;
+import com.android.personalbest.User;
 
 public interface IFirestore{
     public void setName(String name);
@@ -14,4 +17,7 @@ public interface IFirestore{
     public void initMessageUpdateListener(TextView chatView, String otherUserEmail);
     public void addSentMessageToDatabase(EditText editText, String otherUserEmail);
     public void initMainActivity(MainActivity mainActivity, HomeUI homeUI);
+    public void loginCheckIfUserExists(String otherUserEmail, LoginUI loginUI);
+    public void getToKnowYouCheckIfUserExists(String otherUserEmail, GetToKnowYouUI getToKnowYouUI);
+    public void addUserToFirestore(User user, GetToKnowYouUI getToKnowYouUI);
 }
